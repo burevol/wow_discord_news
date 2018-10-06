5ba10035d749aade7d5436d6298d44f735415254
alexum <alexum@webfrontend.dom.local>
2018-10-06 19:09:04 +0300

    Переписан код взаимодействия с БД, настройки выведены в конфигурационный файл

--- wowmysql.py
+++ wowmysql.py
@@ -1,154 +1,73 @@
 import MySQLdb
+import configparser
 
-DBHOST =  '192.168.88.4'
-DBUSER = 'wow_api'
-DBPASSWD =  '^6N9AodseNl|*7ulqCI6'
-DB =  'wow_database'
-
-
-def insert_news(timestamp,type,character,item_id,achievement,achievement_icon):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-
-    cur = db.cursor()
-
-    sql = "INSERT INTO news(timestamp,type,character_name,achievement,item_id) VALUES ('%d','%s','%s','%s','%d')"%(timestamp,type,character,achievement,item_id)
-    try:
-        cur.execute(sql)
-        db.commit()
-    except:
-        db.rollback()
-
-    db.close()
-    if achievement != '':
-        icon = get_achievement_icon(achievement)
+class MySqlConnector():
+    def __init__(self,parent = None):
+        try:
+            config = configparser.ConfigParser()
+            config.read('wowdiscord.conf')
+            self.dbhost = config['Config']['DBHOST']
+            self.dbuser = config['Config']['DBUSER']
+            self.dbpasswd = config['Config']['DBPASSWD']
+            self.db = config['Config']['DB']
+        except:
+            print('Не удалось прочитать конфигурационный файл. Убедитесь в его наличии в папке программы и его правильной структуре.')
+    def connect_db()
+        return MySQLbd.connect(host = self.dbhost, user = self.dbuser, passwd = self.dbpasswd, db = self.db, use_unicode = True, charset = 'utf8')
+    def update_query(text)
+        db = connect_db()
+        cur = db.cursor()
+        try:
+            cur.execute(text)
+            db.commit()
+        except:
+            db.rollback()
+        db.close()
+    def execute_query(text)
+        db = connect_db()
+        cur = db.cursor()
+        row_count = cur.execute(text)
+        db.close()
+        if row_count == 0:
+            return None
+        else:
+            row = cur.fetchone()
+            return row[0]
+    def execute_query_all(text)
+        db = connect_db()
+        cur = db.cursor()
+        cur.execute(text)
+        db.close()
+        return cur.fetchall()
+    def insert_news(timestamp,type,character,item_id,achievement,achievement_icon):
+        sql = "INSERT INTO news(timestamp,type,character_name,achievement,item_id) VALUES ('%d','%s','%s','%s','%d')"%(timestamp,type,character,achievement,item_id)
+        update_query(sql)
+        if achievement != '':
+            icon = get_achievement_icon(achievement)
         if icon == '':
             set_achievement_icon(achievement,achievement_icon)
-
-def get_achievement_icon(achievment):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-    cur = db.cursor()
-    sql = "SELECT icon FROM achievments WHERE achievment = '%s'"%(achievment)
-    row_count = cur.execute(sql)
-    db.close()
-    if row_count == 0:
-        return ''
-    else:
-        row = cur.fetchone()
-        return row[0]
-
-def set_achievement_icon(achievment, icon):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-    cur = db.cursor()
-    sql = "INSERT INTO achievments(achievment,icon) VALUES('%s','%s')"%(achievment,icon)
-    try:
-        cur.execute(sql)
-        db.commit()
-    except:
-        db.rollback()
-    db.close()
-
-
-
-def get_maximum_timestamp():
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB)
-
-    cur = db.cursor()
-    sql = "SELECT MAX(timestamp) AS timestamp FROM news"
-    row_count = cur.execute(sql)
-    row = cur.fetchone()
-    db.close()
-    if row[0] == None:
-        return 0
-    else:
-        return row[0]
-
-def set_item_info(itemId,item_name,icon):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-    cur = db.cursor()
-    print (item_name)
-    sql = 'INSERT INTO items(item_id,item_name,icon) VALUES ("%d","%s","%s")'%(itemId,item_name,icon)
-    try:
-        cur.execute(sql)
-        db.commit()
-    except:
-        db.rollback()
-    db.close()
-
-def get_item_info(itemId):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-
-
-    sql = "SELECT item_name FROM items WHERE item_id = '%d'"%(itemId)
-    cur = db.cursor()
-    row_count = cur.execute(sql)
-    row = cur.fetchone()
-    db.close()
-    if row_count == 0:
-        return ''
-    else:
-        return row[0]
-
-
-def get_item_icon(itemId):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-
-
-    sql = "SELECT icon FROM items WHERE item_id = '%d'"%(itemId)
-    cur = db.cursor()
-    row_count = cur.execute(sql)
-    row = cur.fetchone()
-    db.close()
-    if row_count == 0:
-        return ''
-    else:
-        return row[0]
-
-
-def get_new_messages_from_db():
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-    sql = "SELECT * FROM news WHERE posted = '0'"
-    cur =db.cursor()
-    cur.execute(sql)
-    db.close()
-    return cur.fetchall()
-
-def mark_as_posted(list_ids):
-    db = MySQLdb.connect(host = DBHOST,
-                     user = DBUSER,
-                     passwd = DBPASSWD,
-                     db = DB, use_unicode=True, charset="utf8")
-
-    format_strings = ','.join(['%s'] * len(list_ids))
-    cur =db.cursor()
-    cur.execute("UPDATE news SET posted = 1 WHERE news_id IN (%s)" % format_strings,
-                tuple(list_ids))
-    db.commit()
-    db.close()
-
-#get_maximum_timestamp()
-
-#insert_news(12312331,'obtain',12345,54412)
+    def set_achievement_icon(achievement, icon):
+        sql·=·"INSERT·INTO·achievments(achievment,icon)·VALUES('%s','%s')"%(achievment,icon)
+        update_query(sql)
+    def set_item_info(itemId,item_name,icon):
+        sql·=·'INSERT·INTO·items(item_id,item_name,icon)·VALUES·("%d","%s","%s")'%(itemId,item_name,icon)
+        update_query(sql)
+    def mark_as_posted(list_ids):
+        format_strings = ','.join(['%s'] * len(list_ids))
+        sql = "UPDATE news SET posted = 1 WHERE news_id IN (%s)" % format_strings, tuple(list_ids))
+        update_query(sql)
+    def get_achievement_icon(achievment):
+        sql = "SELECT icon FROM achievments WHERE achievment = '%s'"%(achievment)
+        return execute_query(sql)
+    def get_maximum_timestamp():
+        sql = "SELECT MAX(timestamp) AS timestamp FROM news"
+        return execute_quiery(sql)
+    def get_item_info(itemId):
+        sql = "SELECT item_name FROM items WHERE item_id = '%d'"%(itemId)
+        return execute_query(sql)
+    def get_item_icon(itemId):
+        sql = "SELECT icon FROM items WHERE item_id = '%d'"%(itemId)
+        return execute_query(sql)
+    def get_new_messages_from_db():
+        sql = "SELECT * FROM news WHERE posted = '0'"
+        return execute_query_all()
