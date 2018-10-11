75117f33325b8b362c8520a45413f5d43635d216
burevol <wolfshoul@gmail.com>
2018-10-11 18:04:49 +0300

    Add class MysqlObjects, create code for table races

---
+++ wow_test.py
@@ -0,0 +1,17 @@
+from wd_wowdiscord import WowDiscord
+from wd_mysql import MySqlOperations
+from wd_mysqlobjects import MysqlObjects,Races
+
+if __name__ == '__main__':
+    wd = WowDiscord()
+    with MySqlOperations(wd.cf) as mysql:
+        races = Races(mysql)
+        races.clear_table()
+        print(len(races))
+
+        for race in wd.get_races():
+            races[race['id']] = race
+        print(len(races))
+        print(races['Орк'])
+        print(races[28])
+        print(races['Гномэ'])
