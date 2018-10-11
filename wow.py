95bae3c44fd09b9bd97ce883dc1646121772ee35
burevol <wolfshoul@gmail.com>
2018-10-11 14:34:15 +0300

    Add command line argument parsing

--- wow.py
+++ wow.py
@@ -1,4 +1,10 @@
+import sys
+import argparse
 from wd_wowdiscord import WowDiscord
 
-wd = WowDiscord()
-wd.read_news()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c",default = 'wowdiscord.conf')
+    args = parser.parse_args()
+    wd = WowDiscord(args.c)
+    wd.read_news()
